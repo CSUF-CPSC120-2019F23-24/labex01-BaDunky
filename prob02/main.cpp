7fe922f5935957c7f98bea2388d2ab5322ee258d
BaDunky <arturitosilva20@hotmail.com>
2019-08-24 14:32:58 -0700

    changed names

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -4,18 +4,18 @@
 
 int main()
 {
-  double hours, rate, pay;
+  double bars, rate, pay;
   std::cout << "Welcome to the Earnings Calculator!" << std::endl;
   // Get the number of hours worked.
   std::cout << "How many candy bars did you sell? ";
-  std::cin >> hours;
+  std::cin >> bars;
 
   // Get the hourly pay rate.
   std::cout << "How much does the organization earn for each bar sold? ";
   std::cin >> rate;
 
   // Calculate the pay.
-  pay = hours * rate;
+  pay = bars * rate;
 
   // Display the pay.
   std::cout << "Congratulations! You have earned $" << pay << " for the organization!" << std::endl;
